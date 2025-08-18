edca74a10bd4c181161fdca199697b4fc7b1b8dd
Nathan Stevens <nathan.stevens@instras.com>
2025-08-18 12:51:54 -0400

    Update the spotify api jar to latest and other minor UI changes

--- src/cassetteflow/SpotifyAppInfo.java
+++ src/cassetteflow/SpotifyAppInfo.java
@@ -7,7 +7,7 @@ package cassetteflow;
  */
 public final class SpotifyAppInfo {
     static final String CLIENT_ID = "dc143ecbc3aa42a5aa6505be98169dfd";
-    static final String CLIENT_SECRET = "17615fac4ba04ac9b9b0a1188034a6fe";
+    static final String CLIENT_SECRET = "";
     static final String REDIRECT_URI = "http://localhost:3000";
     
     private SpotifyAppInfo() {}  
