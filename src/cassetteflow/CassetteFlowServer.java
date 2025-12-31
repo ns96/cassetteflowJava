4669cf1d37d8d0d63e75933cd23e70ef570eceb0
Nathan Stevens <nathan.stevens@instras.com>
2025-12-31 13:27:05 -0500

    Bug fix for returning duplicates when searching for to audio files and dct enpoint added to server component

--- src/cassetteflow/CassetteFlowServer.java
+++ src/cassetteflow/CassetteFlowServer.java
@@ -21,7 +21,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * This is class implements a simple http server for testing the ESP32LyraT Client
+ * This is class implements a simple http server for testing the ESP32LyraT
+ * Client
  * Code
  * 
  * @author Nathan
@@ -30,90 +31,93 @@ public class CassetteFlowServer {
     private final int ENCODE = 0;
     private final int DECODE = 1;
     private final int PASS = 2;
-    
+
     private int currentMode = DECODE;
-    
+
     private HttpServer server;
-    
+
     private CassetteFlow cassetteFlow;
-    
+
     // the tape ID for the input files to be created
     private String tapeID;
-    
+
     // array list for storing the mp3 ides associated with create request
     private ArrayList<String> sideAList;
     private ArrayList<String> sideBList;
-    
+
     // the mute time between tracks
     private int mute = 0;
-    
+
     private boolean readRawData = false;
-        
+
     /**
      * Main constructor which starts the server
-     * @throws IOException 
+     * 
+     * @throws IOException
      */
     public CassetteFlowServer() throws IOException {
-        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(10);
-        
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
         server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8192), 0);
-        
+
         server.createContext("/", new SetModeHandler());
         server.createContext("/mp3db", new getMp3DBHandler());
         server.createContext("/tapedb", new getTapeDBHandler());
         server.createContext("/info", new getInfoHandler());
         server.createContext("/raw", new getRawHandler());
+        server.createContext("/dct", new getDCTHandler());
         server.createContext("/create", new createHandler());
         server.createContext("/start", new startHandler());
         server.createContext("/play", new playHandler());
         server.createContext("/stop", new stopHandler());
-        
+
         // define endpoint for interacting with the cassetteflow desktop program
         server.createContext("/dcv", new DecodeViewHandler()); // send the UI
         server.createContext("/dcs", new DecodeStateHandler()); // get the decode state json
         server.createContext("/dcc", new DecodeCommandHandler()); // send a commend to decorder
-         
+
         server.setExecutor(threadPoolExecutor);
         server.start();
-        
+
         System.out.println("Cassette Flow Server Started ...");
     }
-    
+
     /**
      * Set the cassetteflow object
      * 
-     * @param cassetteFlow 
+     * @param cassetteFlow
      */
     public void setCassetteFlow(CassetteFlow cassetteFlow) {
         this.cassetteFlow = cassetteFlow;
     }
-      
+
     /**
      * Stop the http server
      */
     public void stop() {
         server.stop(0);
     }
-    
+
     /**
      * Get all the params from a url
      * 
      * @param url
      * @return
-     * @throws UnsupportedEncodingException 
+     * @throws UnsupportedEncodingException
      */
     public Map<String, String> splitQuery(String query) throws UnsupportedEncodingException {
         Map<String, String> queryPairs = new LinkedHashMap<>();
         String[] pairs = query.split("&");
-        
+
         for (String pair : pairs) {
             int idx = pair.indexOf("=");
-            queryPairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+            queryPairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+                    URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
         }
-        
+
         return queryPairs;
     }
-    
+
     public synchronized void sendResponse(HttpExchange he, String response) {
         try {
             byte[] data = response.getBytes("UTF-8");
@@ -126,21 +130,21 @@ public class CassetteFlowServer {
             Logger.getLogger(CassetteFlowServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     // class to handle setting the mode either decode or encode
     private class SetModeHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = false;
             String query = he.getRequestURI().getQuery();
-            
+
             Map params = splitQuery(query);
             String response = "Set Mode: " + params;
-            
+
             sendResponse(he, response);
         }
     }
-    
+
     // class to handle getting the mp3 database
     private class getMp3DBHandler implements HttpHandler {
         @Override
@@ -150,7 +154,7 @@ public class CassetteFlowServer {
             sendResponse(he, response);
         }
     }
-    
+
     // class to handle getting the mp3 database
     private class getTapeDBHandler implements HttpHandler {
         @Override
@@ -160,14 +164,14 @@ public class CassetteFlowServer {
             sendResponse(he, response);
         }
     }
-    
+
     // class to handle getting information
     private class getInfoHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = false;
             String response;
-            
+
             switch (currentMode) {
                 case DECODE:
                     response = "DECODE " + cassetteFlow.getRawLineRecord();
@@ -180,35 +184,35 @@ public class CassetteFlowServer {
                             "," + cassetteFlow.currentAudioID;
                     break;
             }
-            
+
             sendResponse(he, response);
         }
     }
-    
+
     // class to handle getting continous information
     private class getRawHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = true;
-            
+
             Thread thread = new Thread("Server Raw Reader Thread") {
                 @Override
                 public void run() {
                     try {
                         he.sendResponseHeaders(200, 0); // specify a length of 0 to use chunking
                         OutputStream os = he.getResponseBody();
-                        
+
                         while (readRawData) {
                             // the \r\n must be added for chunking to work
                             String response = "\r\n" + cassetteFlow.getRawLineRecord();
-                            
+
                             byte[] data = response.getBytes("UTF-8");
                             os.write(data);
                             os.flush();
-                            
+
                             Thread.sleep(200);
                         }
-                        
+
                         os.close();
                     } catch (Exception ex) {
                         System.out.println("Connection closed ...");
@@ -218,96 +222,139 @@ public class CassetteFlowServer {
             thread.start();
         }
     }
-    
+
+    // class to handle getting continous information with dct translation
+    private class getDCTHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            readRawData = true;
+
+            Thread thread = new Thread("Server DCT Reader Thread") {
+                @Override
+                public void run() {
+                    try {
+                        he.sendResponseHeaders(200, 0); // specify a length of 0 to use chunking
+                        OutputStream os = he.getResponseBody();
+
+                        while (readRawData) {
+                            String rawLine = cassetteFlow.getRawLineRecord();
+                            String response;
+
+                            if (rawLine != null && rawLine.startsWith("DCT")) {
+                                response = "\r\n" + rawLine + "\n --> " + cassetteFlow.getCurrentLineRecord();
+                            } else {
+                                response = "\r\n" + rawLine;
+                            }
+
+                            byte[] data = response.getBytes("UTF-8");
+                            os.write(data);
+                            os.flush();
+
+                            Thread.sleep(200);
+                        }
+
+                        os.close();
+                    } catch (Exception ex) {
+                        System.out.println("Connection closed ...");
+                    }
+                }
+            };
+            thread.start();
+        }
+    }
+
     // class to handle creating the input files for encoding
     private class createHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = false;
-            
+
             String query = he.getRequestURI().getQuery();
-            
+
             Map params = splitQuery(query);
             extractInformation(params);
-            
+
             String response = "Creating Input File: " + params;
             sendResponse(he, response);
         }
-        
+
         /**
          * Extract the data from the param string
-         * @param params 
+         * 
+         * @param params
          */
         private void extractInformation(Map params) {
             String side = params.get("side").toString();
             mute = Integer.parseInt(params.get("mute").toString());
             String data[] = params.get("data").toString().split(",");
-            
+
             tapeID = data[0];
-            
+
             // get the mp3 ids
             ArrayList<String> mp3Ids = new ArrayList<>();
-            for(int i = 1; i < data.length; i++) {
+            for (int i = 1; i < data.length; i++) {
                 mp3Ids.add(data[i]);
             }
-            
-            if(side.equals("A")) {
+
+            if (side.equals("A")) {
                 sideAList = mp3Ids;
             } else {
                 sideBList = mp3Ids;
             }
         }
     }
-        
+
     // class to start the encoding process
     private class startHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = false;
-            
+
             String query = he.getRequestURI().getQuery();
-            
+
             Map params = splitQuery(query);
             String side = params.get("side").toString();
-            
+
             String message = startEncoding(side);
-            
+
             String response = "Starting encoding of input file: " + params + " || " + message;
             sendResponse(he, response);
         }
-        
+
         /**
          * Start the encoding for the indicate side of the tape
-         * @param side 
+         * 
+         * @param side
          */
         private String startEncoding(String side) {
             final ArrayList<String> sideList = (side.equals("A")) ? sideAList : sideBList;
-            
-            if(sideList != null) {
+
+            if (sideList != null) {
                 cassetteFlow.currentTapeID = tapeID;
-                
+
                 // test this in a thread
                 Thread thread = new Thread("Server Encoder Thread") {
                     @Override
                     public void run() {
-                        for(int i =0; i < sideList.size(); i++) {
-                            int trackNumber =  i + 1;
+                        for (int i = 0; i < sideList.size(); i++) {
+                            int trackNumber = i + 1;
                             String mp3Id = sideList.get(i);
                             cassetteFlow.currentAudioCount = trackNumber;
                             cassetteFlow.currentAudioID = mp3Id;
-                            
-                            System.out.println("Server Encoding Track # " + trackNumber + " / " + mp3Id);  
+
+                            System.out.println("Server Encoding Track # " + trackNumber + " / " + mp3Id);
                             try {
                                 Thread.sleep(5000);
-                            } catch (InterruptedException ex) { }
+                            } catch (InterruptedException ex) {
+                            }
                         }
-                        
+
                         // change the trackNumber to -1 to indicate we are done with encoding
                         cassetteFlow.currentAudioCount = -1;
                     }
                 };
                 thread.start();
-                
+
                 return "OK";
             } else {
                 // indiicate to the client that something is wrong
@@ -315,35 +362,36 @@ public class CassetteFlowServer {
             }
         }
     }
-    
+
     // class to handle playing the mp3 indicated in the input file
     private class playHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = false;
-            
+
             String query = he.getRequestURI().getQuery();
-            
+
             Map params = splitQuery(query);
             String response = "Playing mp3s from input file: " + params;
-            
+
             sendResponse(he, response);
         }
     }
-    
+
     // class to handle getting information
     private class stopHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = false;
-            
+
             String response = "Stopping Encoding or Playing ...";
             sendResponse(he, response);
         }
     }
-    
+
     /**
-     * Handles serving mobile page which allows for veiwing of currently decoding data
+     * Handles serving mobile page which allows for veiwing of currently decoding
+     * data
      * 
      */
     private class DecodeViewHandler implements HttpHandler {
@@ -388,7 +436,7 @@ public class CassetteFlowServer {
             }
         }
     }
-      
+
     private class DecodeStateHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
@@ -406,7 +454,7 @@ public class CassetteFlowServer {
             os.close();
         }
     }
-   
+
     /**
      * Class to send commands to the decoding backend
      */
@@ -414,46 +462,46 @@ public class CassetteFlowServer {
         @Override
         public void handle(HttpExchange he) throws IOException {
             readRawData = false;
-            
+
             String query = he.getRequestURI().getQuery();
             Map params = splitQuery(query);
             String command = params.get("c").toString();
             cassetteFlow.runDecodeCommand(command);
-            
+
             String response = "Decoding command received " + command;
             sendResponse(he, response);
         }
     }
-    
+
     /**
      * Used by client to test encoding
      */
     public void testEncode() {
-        
+
     }
-    
+
     /**
      * Method to run micro server independently of the GUI application
      * 
-     * @param args 
+     * @param args
      */
     public static void main(String[] args) {
         try {
             // init the cassette playey and object to start the minimodem program
-            CassetteFlow cf = new CassetteFlow();  
+            CassetteFlow cf = new CassetteFlow();
             CassettePlayer cp = new CassettePlayer(cf, null);
             cp.setRawLineRecordOnly(true);
-            
+
             // trying starting minimodem in try block in case it's not installed
             try {
                 cp.startMinimodem(0);
-            } catch(IOException iex) {
+            } catch (IOException iex) {
                 System.out.println("No Minimodem program found ...\n");
             }
-            
+
             CassetteFlowServer cfs = new CassetteFlowServer();
             cfs.setCassetteFlow(cf);
-            
+
             // indicate where to access the line records
             System.out.println("GoTo: http://localhost:8192/raw to access line records");
         } catch (IOException ex) {
